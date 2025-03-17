6eeea8cda3a79a16496d9a04d8e4dbfd25cd152c
Nishchal-Vashisht-yoti <nishchal.vashisht@yoti.com>
2025-03-17 08:56:17 +0530

    changes

--- force-app/main/default/lwc/category/category.js
+++ force-app/main/default/lwc/category/category.js
@@ -10,4 +10,6 @@ export default class Category extends LightningElement {
     topicLink = [{'id':1,'name':'Topic link'},{'id':2,'name':'Topic link'},{'id':3,'name':'Topic link'},{'id':4,'name':'Topic link'}];
     titleLink =[{'id':1,'name':'This is the title of an article'},{'id':2,'name':'This is the title of an article'},{'id':3,'name':'This is the title of an article'},{'id':4,'name':'This is the title of an article'}];
     topicList = [{'id':1,'name':'Topic'},{'id':2,'name':'Topic'},{'id':3,'name':'Topic'},{'id':4,'name':'Topic'}];
+
+    
 }
--- force-app/main/default/lwc/categoryHeading/categoryHeading.js
+++ force-app/main/default/lwc/categoryHeading/categoryHeading.js
@@ -1,7 +1,41 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,wire } from 'lwc';
 import righticon from '@salesforce/resourceUrl/rightIcon';
+import searchKnowledgeArticles from '@salesforce/apex/YotiSupportSiteController.searchKnowledgeArticles';
+import { CurrentPageReference } from 'lightning/navigation';
+
 
 export default class CategoryHeading extends LightningElement {
     rightIconT =  righticon;
+    type = ''
+    product = ''
     
+    @wire(CurrentPageReference)
+    getStateParameters(currentPageReference) {
+       if (currentPageReference) {
+            this.type = currentPageReference.state?.type;
+             this.product = currentPageReference.state?.product;
+       }
+    }
+
+    connectedCallback() {
+        // Get the query string from the URL
+        const urlParams = new URLSearchParams(window.location.search);
+
+        // Fetch specific parameters
+        this.type = urlParams.get('type'); // Returns 'value1'
+        this.product = urlParams.get('product'); // Returns 'value2'
+
+        if(this.type == null){
+            this.type = 'Dummy';
+        }
+        if(this.product == null){
+            this.product = 'Dummy';
+        }
+
+        console.log('param1:', this.param1Value);
+        console.log('param2:', this.param2Value);
+    }
+
+
+
 }
